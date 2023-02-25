3067605c844f72296553569484b214e293dfed98
Michael Davis <mdavis@notasketchylink.com>
2023-02-25 14:06:52 -0600

    updated path on edl-update.sh

--- edl-update.sh
+++ edl-update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Execute the first python script
-/bin/python3 /scripts/python3/edl-updater/edl-downloader2.py
+/bin/python3 /scripts/edl-updater/edl-downloader2.py
 
 # Execute the second python script
-/bin/python3 /scripts/python3/edl-updater/edl-sanitizer.py
+/bin/python3 /scripts/edl-updater/edl-sanitizer.py
 
 # Execute the third python script
-/bin/python3 /scripts/python3/edl-updater/edl-ip-handler2.py
+/bin/python3 /scripts/edl-updater/edl-ip-handler2.py
 
 # Execute the fourth python script
-/bin/python3 /scripts/python3/edl-updater/edl-domain-handler2.py
+/bin/python3 /scripts/edl-updater/edl-domain-handler2.py
